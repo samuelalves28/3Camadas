7ef49e164e66deeee88c28c9dbd159c3ccb90a8e
samuca.alves <samucaalves28@gmail.com>
2025-03-25 14:16:01 -0300

    Criando Data Context

---
+++ src/Data/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+public class DataBaseContext(DbContextOptions<DataBaseContext> options) : DbContext(options)
+{
+    public DbSet<T> GetDbSet<T>() where T : class => Set<T>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        var entityTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .Where(t =>
+                t.IsClass &&
+                !t.IsAbstract &&
+                t.IsPublic &&
+                t.Namespace == "Business.Models");
+
+        foreach (var type in entityTypes)
+            modelBuilder.Entity(type);
+    }
+}
